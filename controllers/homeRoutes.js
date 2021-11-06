f2216ee2f7ef3b90908f6b17fd376f6379ed233e
Nelson Rivera <riveranelson99@gmail.com>
2021-11-05 22:26:40 -0500

    Finalized seeds folder to work with mock data

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,31 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Comment, Post } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+        {
+          model: Comment,
+          attributes: ['username']
+        }
+      ],
+    });
+
+    const posts = postData.map(post => post.get({ plain: true }));
+
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 
 router.get('/', async (req, res) => {
   try {
--- seeds/index.js
+++ seeds/index.js
@@ -1,20 +1,26 @@
-const comments = require('./commentData');
-const posts = require('./postData');
-const users = require('./userData');
-
 const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
 
-const seedAll = async () => {
+const users = require('./userData');
+const posts = require('./postData');
+const comments = require('./commentData');
+
+
+const seedDb = async () => {
     await sequelize.sync({ force: true });
-    console.log('\n----- DATABASE SYNCED -----\n')
-    await comments();
-    console.log('\n----- COMMENTS SEEDED -----\n');
-    await posts();
-    console.log('\n----- POSTS SEEDED -----\n');
-    await users();
+    console.log('\n----- DATABASE SYNCED -----\n');
+
+    await User.bulkCreate(users);
+    console.error();
     console.log('\n----- USERS SEEDED -----\n');
+
+    await Post.bulkCreate(posts);
+    console.log('\n----- POSTS SEEDED -----\n');
+
+    await Comment.bulkCreate(comments);
+    console.log('\n----- COMMENTS SEEDED -----\n');
 
     process.exit(0);
 };
 
-seedAll();
+seedDb();
